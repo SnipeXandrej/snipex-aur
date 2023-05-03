9412cab4fb1be708ac6076f7aef4057ddc1e95ff
SnipeX_ <andrej.halv@gmail.com>
2023-05-03 18:26:30 +0200

    scripts: remove --makepkg-conf option
    There is a flaw in aurutils/devtools that replaces the $MAKEFLAGS
    and $PACKAGER vars in the supplied makepkg config file with the
    ones on the HOST system.

--- aur-packages.sh
+++ aur-packages.sh
@@ -2,12 +2,11 @@
 WORK_DIR=$(pwd)
 CHROOT_DIR=/tmp_storage/linux/buildchroot
 PKG_LIST=${WORK_DIR}/aur-packages.list
-MAKEPKG_CONF=/etc/aurutils/makepkg-snipex.conf
 
 _packages=$(cat ${PKG_LIST} | grep -E "^[^#]" | wc -l)
 for ((i=1; i<=_packages; i++));
 do
     echo " "
     echo $i/$_packages Building "${$(cat ${PKG_LIST} | grep -E "^[^#]" | head -$i | tail +$i)%#*}"
-    aur sync --noview --chroot -D ${CHROOT_DIR} -d snipex $(echo "${$(cat ${PKG_LIST} | grep -E "^[^#]" | head -$i | tail +$i)%#*}") --makepkg-conf ${MAKEPKG_CONF} --margs --skipinteg --noconfirm
+    aur sync --noview --chroot -D ${CHROOT_DIR} -d snipex $(echo "${$(cat ${PKG_LIST} | grep -E "^[^#]" | head -$i | tail +$i)%#*}") --margs --skipinteg --noconfirm
 done
