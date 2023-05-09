f75c505cc3b26243fbe4b9a0ad070e616ed798b5
SnipeX_ <andrej.halv@gmail.com>
2023-05-09 19:25:40 +0200

    scripts: add AURDEST= environment variable

--- aur-packages-git.sh
+++ aur-packages-git.sh
@@ -3,11 +3,12 @@ WORK_DIR=$(pwd)
 CHROOT_DIR=/tmp_storage/linux/buildchroot
 PKG_LIST=${WORK_DIR}/aur-packages-git.list
 MAKEPKG_CONF=/etc/makepkg.conf
+AUR_DIR=${WORK_DIR}/AUR
 
 _packages=$(cat ${PKG_LIST} | grep -E "^[^#]" | wc -l)
 for ((i=1; i<=_packages; i++));
 do
     echo " "
     echo $i/$_packages Building "${$(cat ${PKG_LIST} | grep -E "^[^#]" | head -$i | tail +$i)%#*}"
-    aur sync --noview --chroot -D ${CHROOT_DIR} --rebuild -d snipex $(echo "${$(cat ${PKG_LIST} | grep -E "^[^#]" | head -$i | tail +$i)%#*}") --makepkg-conf ${MAKEPKG_CONF} --margs --skipinteg --noconfirm
+    AURDEST=$AUR_DIR aur sync --noview --chroot -D ${CHROOT_DIR} --rebuild -d snipex $(echo "${$(cat ${PKG_LIST} | grep -E "^[^#]" | head -$i | tail +$i)%#*}") --makepkg-conf ${MAKEPKG_CONF} --margs --skipinteg --noconfirm
 done
