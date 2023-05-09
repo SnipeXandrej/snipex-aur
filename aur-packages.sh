a8b08b0fec807bf8ad61151c39404f26c03cde8f
SnipeX_ <andrej.halv@gmail.com>
2023-05-09 19:35:09 +0200

    scripts: replace/improve code responsible for removing comments

--- aur-packages.sh
+++ aur-packages.sh
@@ -9,6 +9,6 @@ _packages=$(cat ${PKG_LIST} | grep -E "^[^#]" | wc -l)
 for ((i=1; i<=_packages; i++));
 do
     echo " "
-    echo $i/$_packages Building "${$(cat ${PKG_LIST} | grep -E "^[^#]" | head -$i | tail +$i)%#*}"
-    AURDEST=$AUR_DIR aur sync --noview --chroot -D ${CHROOT_DIR} -d snipex $(echo "${$(cat ${PKG_LIST} | grep -E "^[^#]" | head -$i | tail +$i)%#*}") --makepkg-conf ${MAKEPKG_CONF} --margs --skipinteg --noconfirm
+    echo $i/$_packages Building "$(cat ${PKG_LIST} | sed 's/#.*//' | grep . | head -$i | tail +$i)"
+    AURDEST=$AUR_DIR aur sync --noview --chroot -D ${CHROOT_DIR} -d snipex "$(cat ${PKG_LIST} | sed 's/#.*//' | grep . | head -$i | tail +$i)" --makepkg-conf ${MAKEPKG_CONF} --margs --skipinteg --noconfirm
 done
